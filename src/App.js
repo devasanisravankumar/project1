5bbb1f1d5bf3a9e2b642849d2e54c7bbfbf52bae
sravan <sravankumar.d@beyondit-services.com>
2023-05-26 18:56:30 +0530

    newbranch
    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Button from "@mui/material/Button"
 
 function sample(){
  return(
    <>
    <h1>Sample</h1>
    <p>This is a sample paragraph.</p>
+   <Button variant="contained" sx={{margin:25}}>click</Button>
    </>
  )
 }
